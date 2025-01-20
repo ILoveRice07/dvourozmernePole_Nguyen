353a7ed967c32faff23a68c94c77458f25d50a5d
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2025-01-20 17:48:19 +0100

    add_row

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 #Vytvoření dvourozměrného pole
 matrix = [[1,2,3],
           [4,5,6],
@@ -5,3 +6,8 @@ matrix = [[1,2,3],
 
 #Přepsání čisla v 2. sloupci a 2. řádku
 matrix[1][1] = 105
+print(matrix)
+
+#Přidání 1 řádku a sloupce
+matrix_append = np.append(matrix,10)
+print(matrix_append)
